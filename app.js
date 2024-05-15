5101e9c54ca6fc46e3828b6cbce4e50731a7cbbe
lidunlife <uchihalidun@gmail.com>
2024-05-15 15:40:18 +0500

    5th-lesson

---
+++ app.js
@@ -0,0 +1,15 @@
+let h1 = document.getElementById(`random`)
+let body = document.querySelector(`body`)
+
+let num = prompt(`Fill in your text`)
+h1.textContent = num
+
+
+h1.style.color = `white`
+h1.style.fontSize = `xxx-large`
+
+body.style.background = `black`
+body.style.display = `flex`
+body.style.justifyContent = `center`
+body.style.alignItems = `center`
+body.style.height = `100vh`
